32b04d04fd03c5c6b7aa78d1663030dc5f6fd988
5abhisheksaxena <5abhisheksaxena@gmail.com>
2020-05-23 21:08:02 +0530

    Update with Part 2 content.
    This commit contains the code of the second video of three-part video series.

--- app/src/main/java/in/abhisheksaxena/simple_calculator/MainActivity.kt
+++ app/src/main/java/in/abhisheksaxena/simple_calculator/MainActivity.kt
@@ -2,6 +2,9 @@ package `in`.abhisheksaxena.simple_calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,5 +12,51 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setupSpinner()
+
+        calculateButton.setOnClickListener{
+            val firstNumberString = numberOneEditText.text.toString()
+            val secondNumberString = numberTwoEditText.text.toString()
+
+            if (firstNumberString.isEmpty()) {
+                Toast.makeText(this, "Enter first number.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if (secondNumberString.isEmpty()) {
+                Toast.makeText(this, "Enter second number.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            val firstNumber = firstNumberString.toDouble()
+            val secondNumber = secondNumberString.toDouble()
+
+            val operation = operatorSpinner.selectedItem.toString()
+
+            val result: Double? = when (operation) {
+                ADD -> firstNumber + secondNumber
+                SUBTRACT -> firstNumber - secondNumber
+                MULTIPLY -> firstNumber * secondNumber
+                DIVIDE -> firstNumber / secondNumber
+                else -> null
+            }
+
+            resultTextView.text = if(result != null) ("Result: $result") else ("Invalid Operation")
+        }
+
+    }
+
+    private fun setupSpinner(){
+        val operators = listOf(ADD, SUBTRACT, MULTIPLY, DIVIDE)
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, operators)
+        operatorSpinner.adapter = adapter
+    }
+
+    companion object{
+        const val ADD = "Add"
+        const val SUBTRACT = "Subtract"
+        const val MULTIPLY = "Multiply"
+        const val DIVIDE = "Divide"
     }
 }
